9c96338121b5863ce44b989ad3fd1650e0b0c837
Jardiel Sousa <jardiel.sousa@gmail.com>
2019-09-22 15:33:12 -0300

    Template resume

---
+++ src/components/Content.vue
@@ -0,0 +1,22 @@
+<template>
+  <article class="resume-wrapper text-center position-relative">
+    <div class="resume-wrapper-inner mx-auto text-left bg-white shadow-lg">
+      <Header></Header>
+      <ResumeBody></ResumeBody>
+    </div>
+  </article>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import ResumeBody from "./ResumeBody.vue";
+
+export default {
+  name: "contentTemplate",
+  components: { Header, ResumeBody }
+}
+</script>
+
+<style>
+
+</style>
